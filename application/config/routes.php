450c1d08a07483c1d87789cc294bdfe84a6cf456
nauman <vuhomework22@gmail.com>
2016-12-20 11:27:47 +0500

    update

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,4 +1,4 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$route['default_controller'] = "Admission";
+$route['default_controller'] = "Admission_11th_reg";
 $route['404_override'] = '';
