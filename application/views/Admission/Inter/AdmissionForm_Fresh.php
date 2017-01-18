dbc8d6e517c6b26018da9b0e5f8999a3cf4c9eef
shahid <shahid1376>
2017-01-18 09:12:49 +0500

    added

--- application/views/Admission/Inter/AdmissionForm_Fresh.php
+++ application/views/Admission/Inter/AdmissionForm_Fresh.php
@@ -1054,18 +1054,14 @@ header("Pragma: no-cache");
 
                             function checks()
                             {
-
-
-                                var status  =  check_NewEnrol_validation();
+                                var status  =  check_NewEnrol_validation_Fresh();
                                 if(status == 0)
                                 {
                                     return false;    
                                 }
                                 else
                                 {
-
                                     //debugger;
-
                                     $.ajax({
 
                                         type: "POST",
