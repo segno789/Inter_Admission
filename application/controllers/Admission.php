d127aeb0f15b94c67854ee02a19042dea8acc7e2
Usama Arshad <usama692@hotmail.com>
2018-01-03 13:04:57 +0500

    updated

--- application/controllers/Admission.php
+++ application/controllers/Admission.php
@@ -967,14 +967,14 @@ class Admission extends CI_Controller {
 
         if($data ['IsNewPic'] == 0)
         {
-          /*  $type = pathinfo(@$data['picpath'], PATHINFO_EXTENSION); 
+            $type = pathinfo(@$data['picpath'], PATHINFO_EXTENSION); 
             @$image_path_selected = 'data:image/' . $type . ';base64,' . base64_encode(file_get_contents(@$data['picpath']));
-            $pdf->Image(@$data['picpath'],6.5, 1.30+$Y, 0.95, 1.0, "JPG");*/
+            $pdf->Image(@$data['picpath'],6.5, 1.30+$Y, 0.95, 1.0, "JPG");
         }
 
         else  if($data ['IsNewPic'] == 1 && Session == 1)
         {
-           $pdf->Image(GET_PRIVATE_IMAGE_PATH.$data['picpath'],6.5, 1.30+$Y, 0.95, 1.0, "JPG");
+            $pdf->Image(GET_PRIVATE_IMAGE_PATH.$data['picpath'],6.5, 1.30+$Y, 0.95, 1.0, "JPG");
         }
 
         $pdf->Image("assets/img/logo2.png",0.4, 0.2, 0.65, 0.65, "PNG");
@@ -1812,9 +1812,9 @@ class Admission extends CI_Controller {
         {
             if($data ['IsNewPic'] == 0)
             {
-               /* $type = pathinfo(@$data['picpath'], PATHINFO_EXTENSION); 
+                $type = pathinfo(@$data['picpath'], PATHINFO_EXTENSION); 
                 @$image_path_selected = 'data:image/' . $type . ';base64,' . base64_encode(file_get_contents(@$data['picpath']));
-                $pdf->Image($image_path_selected,6.5, 10.65+$Y, 0.95, 1.0, "JPG");    */
+                $pdf->Image($image_path_selected,6.5, 10.65+$Y, 0.95, 1.0, "JPG");
             }
             else if($data ['IsNewPic'] == 1)
             {
